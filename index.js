38dab4dd7350f642730f18cb8f15340f048a0362
stranger583 <zed042760@gmail.com>
2022-10-21 12:47:16 +0800

    架構

---
+++ index.js
@@ -0,0 +1,43 @@
+new Vue({
+    el:"#app",
+    data:{
+        lists:[
+            {
+                img:"./img/ing1.png",
+                text:"UX Design"
+            },
+            {
+                img:"./img/ing2.png",
+                text:"UI Design"
+            },
+            {
+                img:"./img/ing3.png",
+                text:"website development"
+            },
+            {
+                img:"./img/ing4.png",
+                text:"mobile app development"
+            },
+            {
+                img:"./img/ing5.png",
+                text:"ecommerce"
+            },
+            {
+                img:"./img/ing6.png",
+                text:"customer loyalty"
+            },
+            {
+                img:"./img/ing7.png",
+                text:"digital transformation"
+            },
+            {
+                img:"./img/ing8.png",
+                text:"digital marketing"
+            },
+            {
+                img:"./img/ing9.png",
+                text:"BRANDING"
+            },
+        ]
+    },
+})
